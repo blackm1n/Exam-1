67d1b446c54ce736fe70b5aa8930ecbc39052748
Black <modnyak0001@mail.ru>
2022-11-28 17:00:52 +1000

    Added Count3orLess

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿string[] array = {"hello", "hi", "bye", "marshmallow"};
+
+int Count3orLess(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+            count += 1;
+    }
+    return count;
+}
+
+Console.WriteLine(Count3orLess(array));
