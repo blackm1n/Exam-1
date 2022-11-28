7cf0c80ad3cfd0421259c170fc3443ee2749a161
Black <modnyak0001@mail.ru>
2022-11-28 17:02:16 +1000

    Added Arrayof3orLess

--- Program.cs
+++ Program.cs
@@ -11,4 +11,24 @@ int Count3orLess(string[] array)
     return count;
 }
 
-Console.WriteLine(Count3orLess(array));
+string[] Arrayof3orLess(string[] array)
+{
+    string[] result = new string[Count3orLess(array)];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            result[j] = array[i];
+            j += 1;
+        }
+    }
+    return result;
+}
+
+string[] output = Arrayof3orLess(array);
+
+for (int i = 0; i < output.Length; i++)
+{
+    Console.WriteLine($"{output[i]}");
+}
